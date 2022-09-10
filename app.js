d3be643b89531e329e857820f56d360efb65bed7
Alex Le <alexkaile@gmail.com>
2022-09-10 17:42:43 -0400

    web

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -40,15 +40,25 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
+  const filtered = fruitsArray.filter(obj => {
+    return obj.nutritions.sugar > 15
+  })
 
+  const mapped = filtered.map(obj => {
+    return obj.name
+  })
+
+  console.log(filtered)
   // TODO: Create a new HTML element to display your data 
+  const newElement = document.createElement('div');
+  newElement.innerHTML = mapped;
 
+  const newHTML = document.getElementById('result-div');
   // TODO: Append your new element to the page
-
+  newHTML.append(newElement);
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
